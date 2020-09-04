c16db571cc25cff62dfc896883ec4c4b809fea73
David Widmann <devmotion@users.noreply.github.com>
2020-09-04 08:41:01 +0200

    Remove static type parameter

--- src/RuntimeGeneratedFunctions.jl
+++ src/RuntimeGeneratedFunctions.jl
@@ -9,7 +9,7 @@ struct RuntimeGeneratedFunction{uuid,argnames}
         new{uuid,argnames}()
     end
 end
-(f::RuntimeGeneratedFunction{uuid})(args::Vararg{Any,N}) where {uuid,N} = generated_callfunc(f, args...)
+(f::RuntimeGeneratedFunction)(args::Vararg{Any,N}) where N = generated_callfunc(f, args...)
 
 @inline @generated function generated_callfunc(f::RuntimeGeneratedFunction{uuid,argnames},__args...) where {uuid,argnames}
     setup = (:($(argnames[i]) = @inbounds __args[$i]) for i in 1:length(argnames))
