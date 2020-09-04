ebe0e0562db320ed256aa1ae44d013f3eb37fff7
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-09-04 02:34:59 -0400

    working code

--- src/RuntimeGeneratedFunctions.jl
+++ src/RuntimeGeneratedFunctions.jl
@@ -1,5 +1,24 @@
 module RuntimeGeneratedFunctions
 
-# Write your package code here.
+const function_cache = Dict{UInt64,Expr}()
+struct RuntimeGeneratedFunction{uuid,argnames}
+    function RuntimeGeneratedFunction(ex)
+        uuid = hash(ex.args[2])
+        function_cache[uuid] = ex.args[2]
+        argnames = (ex.args[1].args[2:end]...,)
+        new{uuid,argnames}()
+    end
+end
+(f::RuntimeGeneratedFunction{uuid})(args::Vararg{Any,N}) where {uuid,N} = generated_callfunc(f, args...)
+
+@inline @generated function generated_callfunc(f::RuntimeGeneratedFunction{uuid,argnames},__args...) where {uuid,argnames}
+    setup = (:($(argnames[i]) = @inbounds __args[$i]) for i in 1:length(argnames))
+    quote
+        $(setup...)
+        $(function_cache[uuid])
+    end
+end
+
+export RuntimeGeneratedFunction
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,53 @@
-using RuntimeGeneratedFunctions
+using RuntimeGeneratedFunctions, BenchmarkTools
 using Test
 
-@testset "RuntimeGeneratedFunctions.jl" begin
-    # Write your tests here.
+function f(_du,_u,_p,_t)
+    @inbounds _du[1] = _u[1]
+    @inbounds _du[2] = _u[2]
+    nothing
 end
+
+ex1 = :((_du,_u,_p,_t) -> begin
+    @inbounds _du[1] = _u[1]
+    @inbounds _du[2] = _u[2]
+    nothing
+end)
+
+ex2 = :(function f(_du,_u,_p,_t)
+    @inbounds _du[1] = _u[1]
+    @inbounds _du[2] = _u[2]
+    nothing
+end)
+
+ex3 = :(function (_du,_u,_p,_t)
+    @inbounds _du[1] = _u[1]
+    @inbounds _du[2] = _u[2]
+    nothing
+end)
+
+f1 = RuntimeGeneratedFunction(ex1)
+f2 = RuntimeGeneratedFunction(ex2)
+f3 = RuntimeGeneratedFunction(ex3)
+
+du = rand(2)
+u = rand(2)
+p = nothing
+t = nothing
+
+@test_broken f1(du,u,p,t)
+#du == u
+du = rand(2)
+f2(du,u,p,t)
+@test du == u
+du = rand(2)
+@test_broken f3(du,u,p,t)
+#du == u
+
+t1 = @belapsed $f($du,$u,$p,$t)
+#t2 = @belapsed $f1($du,$u,$p,$t)
+t3 = @belapsed $f2($du,$u,$p,$t)
+#t4 = @belapsed $f3($du,$u,$p,$t)
+
+@test_broken t1 ≈ t2
+@test t1 ≈ t3
+@test_broken t1 ≈ t4
