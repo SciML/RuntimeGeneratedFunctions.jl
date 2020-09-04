4ee2192da1d173d11eb2f719aea4b4e043c3073b
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-09-04 02:54:50 -0400

    3 nanosecond?

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,9 +48,9 @@ t1 = @belapsed $f($du,$u,$p,$t)
 t3 = @belapsed $f2($du,$u,$p,$t)
 #t4 = @belapsed $f3($du,$u,$p,$t)
 
-@test_broken t1 ≈ t2 atol = 2e-9
-@test t1 ≈ t3 atol = 2e-9
-@test_broken t1 ≈ t4 atol = 2e-9
+@test_broken t1 ≈ t2 atol = 3e-9
+@test t1 ≈ t3 atol = 3e-9
+@test_broken t1 ≈ t4 atol = 3e-9
 
 function no_worldage()
     ex = :(function f(_du,_u,_p,_t)
