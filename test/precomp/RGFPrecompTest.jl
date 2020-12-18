83790cde497363f957bba9d0ac60062f8354ee58
Dan Padilha <dpad@g.ecc.u-tokyo.ac.jp>
2020-12-18 14:38:55 +0900

    Fix up tests to use the new RuntimeGeneratedFunction constructor instead (and also test with the deprecated macro).

--- test/precomp/RGFPrecompTest.jl
+++ test/precomp/RGFPrecompTest.jl
@@ -1,15 +1,15 @@
 module RGFPrecompTest
     using RuntimeGeneratedFunctions
     RuntimeGeneratedFunctions.init(@__MODULE__)
-
     
     z = 100
     f = @RuntimeGeneratedFunction(:((x,y)->x+y+z))
-    f2 = RuntimeGeneratedFunction(@__MODULE__, :((x,y)->x+y))
-    # f2 = @RuntimeGeneratedFunction(@__MODULE__, :((x,y)->x-y+z))
+    f2 = RuntimeGeneratedFunction(@__MODULE__, :((x,y)->x-y+z))
 
     function build_RGF(ex, mod=@__MODULE__)
-        @RuntimeGeneratedFunction(mod, ex)
+        # This function is used to test out a consumer module (i.e. a module
+        # that calls this module's function) in the RGFPrecompConsumerTest.
+        RuntimeGeneratedFunction(mod, ex)
     end
 
     module Submodule
@@ -20,6 +20,6 @@ module RGFPrecompTest
         f = @RuntimeGeneratedFunction(:((x,y)->x*y+z))
 
         # Define a version in the parent scope (i.e. using the parent module's "z")
-        f2 = @RuntimeGeneratedFunction(parentmodule(@__MODULE__), :((x,y)->x/y+z))  
+        f2 = RuntimeGeneratedFunction(parentmodule(@__MODULE__), :((x,y)->x/y+z))  
     end
 end
