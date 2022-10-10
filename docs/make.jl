bf3104e93b88d8aa8e379b73ee627f66b308d878
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-10-10 07:44:31 -0400

    generate documenter docs from README

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using RuntimeGeneratedFunctions
+using Documenter
+
+dir = @__DIR__() * "/.."
+cp(joinpath(dir, "README.md"), joinpath(dir, "docs", "src", "index.md"), force=true)
+
+makedocs(sitename = "RuntimeGeneratedFunctions.jl",
+         authors = "Chris Rackauckas",
+         modules = [RuntimeGeneratedFunctions],
+         clean = true, doctest = false,
+         strict = [
+             :doctest,
+             :linkcheck,
+             :parse_error,
+             :example_block,
+             # Other available options are
+             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
+         format = Documenter.HTML(analytics = "UA-90474609-3",
+                                  assets = ["assets/favicon.ico"],
+                                  canonical = "https://runtimegeneratedfunctions.sciml.ai/stable/"),
+         pages = ["RuntimeGeneratedFunctions.jl: Efficient Staged Compilation" => "index.md"])
+
+deploydocs(;
+           repo = "github.com/SciML/RuntimeGeneratedFunctions.jl",
+           devbranch = "main")
