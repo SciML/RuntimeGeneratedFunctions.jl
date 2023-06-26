c2d632d3a03ddffb42f2d972c5b1ea08adff6d48
ArnoStrouwen <arno.strouwen@telenet.be>
2023-06-26 03:27:02 +0200

    format

--- docs/make.jl
+++ docs/make.jl
@@ -5,23 +5,23 @@ cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
 cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
 
 makedocs(sitename = "RuntimeGeneratedFunctions.jl",
-         authors = "Chris Rackauckas",
-         modules = [RuntimeGeneratedFunctions],
-         clean = true, doctest = false,
-         strict = [
-             :doctest,
-             :linkcheck,
-             :parse_error,
-             :example_block,
-             # Other available options are
-             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-         ],
-         format = Documenter.HTML(analytics = "UA-90474609-3",
-                                  assets = ["assets/favicon.ico"],
-                                  canonical = "https://docs.sciml.ai/RuntimeGeneratedFunctions/stable/"),
-         pages = [
-             "RuntimeGeneratedFunctions.jl: Efficient Staged Compilation" => "index.md",
-         ])
+    authors = "Chris Rackauckas",
+    modules = [RuntimeGeneratedFunctions],
+    clean = true, doctest = false,
+    strict = [
+        :doctest,
+        :linkcheck,
+        :parse_error,
+        :example_block,
+        # Other available options are
+        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+    ],
+    format = Documenter.HTML(analytics = "UA-90474609-3",
+        assets = ["assets/favicon.ico"],
+        canonical = "https://docs.sciml.ai/RuntimeGeneratedFunctions/stable/"),
+    pages = [
+        "RuntimeGeneratedFunctions.jl: Efficient Staged Compilation" => "index.md",
+    ])
 
 deploydocs(;
-           repo = "github.com/SciML/RuntimeGeneratedFunctions.jl")
+    repo = "github.com/SciML/RuntimeGeneratedFunctions.jl")
