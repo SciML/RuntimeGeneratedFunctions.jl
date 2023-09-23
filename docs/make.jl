4d80829a78cdd0ec48cfe7fd68c0df9576f10de7
Arno Strouwen <arno.strouwen@telenet.be>
2023-09-23 06:42:17 +0200

    Documenter 1.0 upgrade

--- docs/make.jl
+++ docs/make.jl
@@ -8,17 +8,7 @@ makedocs(sitename = "RuntimeGeneratedFunctions.jl",
     authors = "Chris Rackauckas",
     modules = [RuntimeGeneratedFunctions],
     clean = true, doctest = false, linkcheck = true,
-    strict = [
-        :doctest,
-        :linkcheck,
-        :parse_error,
-        :example_block,
-        :cross_references,
-        # Other available options are
-        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-    ],
-    format = Documenter.HTML(analytics = "UA-90474609-3",
-        assets = ["assets/favicon.ico"],
+    format = Documenter.HTML(assets = ["assets/favicon.ico"],
         canonical = "https://docs.sciml.ai/RuntimeGeneratedFunctions/stable/"),
     pages = [
         "RuntimeGeneratedFunctions.jl: Efficient Staged Compilation" => "index.md",
